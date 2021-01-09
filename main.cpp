e666670a4e9c26a2bbf02643d412e96f65468763
AlbertGawin <166648@stud.prz.edu.pl>
2021-01-09 22:24:26 +0100

    Ostatnie poprawki

--- main.cpp
+++ main.cpp
@@ -10,15 +10,18 @@ struct Zegar
 
     Zegar()
     {
+        // Poczatek odliczania zegara
         _start = std::chrono::high_resolution_clock::now();
         czas = std::chrono::seconds(0);
     }
 
     ~Zegar()
     {
+        // Koniec odliczania zegara
         _end = std::chrono::high_resolution_clock::now();
         czas = _end - _start;
 
+        // Wypisanie czasu jaki uplynal na wykonanie funkcji
         float ms = czas.count() * 1000.f;
         std::cout << "czas: " << ms << " milisekund\n\n";
     }
@@ -48,6 +51,7 @@ public:
 
         std::cout << "Sortuje babelkowo...\n";
 
+        // Sortowanie babelkowe
         for (int i = 0; i < dlugosc - 1; i++)
         {
             for (int j = i + 1; j < dlugosc; j++)
@@ -93,7 +97,7 @@ public:
     }
 };
 
-void OdczytajDaneZPlikuDoTablicy(Tablica &tab, std::string sciezka)
+void OdczytajDaneZPlikuDoTablicy(Tablica& tab, std::string sciezka)
 {
     std::fstream plik(sciezka, std::ios::in);
     bool isEmpty = plik.peek() == std::ifstream::traits_type::eof();
@@ -107,13 +111,13 @@ void OdczytajDaneZPlikuDoTablicy(Tablica &tab, std::string sciezka)
     }
     else
     {
-        std::cout << "Plik nie istnieje lub podano zla sciezke" << std::endl;
-        plik.close();
+        std::cout << "Plik nie istnieje, jest pusty lub podano zla sciezke" << std::endl;
     }
+
     plik.close();
 }
 
-void ZapiszTabliceDoPliku(Tablica &tab, std::string sciezka)
+void ZapiszTabliceDoPliku(Tablica& tab, std::string sciezka)
 {
     std::fstream plik;
     plik.open(sciezka, std::ios::out);
@@ -127,8 +131,7 @@ void ZapiszTabliceDoPliku(Tablica &tab, std::string sciezka)
     }
     else
     {
-        std::cout << "Plik nie istnieje lub podano zla sciezke" << std::endl;
-        plik.close();
+        std::cout << "Blad w zapisie pliku" << std::endl;
     }
 
     plik.close();
@@ -136,25 +139,33 @@ void ZapiszTabliceDoPliku(Tablica &tab, std::string sciezka)
 
 int main()
 {
+    // [N] Rozmiar tablicy oraz wyznacznik liczb losowanych od [0 do N]
+    // N podaje uzytkownik w trakcie rozpoczecia programu
     long long N = 0;
     std::cout << "Podaj N: ";   std::cin >> N;
     std::cout << std::endl;
 
+    // Klasa w ktorej jest tablica, jej dlugosc oraz funkcje sortujace
     Tablica tablica(N);
 
+    // Losowanie liczb
     srand((unsigned int)time(NULL));
     for (int i = 0; i < N; i++)
     {
         tablica.dane[i] = rand() % (N + 1);
     }
+
+    // Zapisanie nieposortowanej tablicy do pliku
     ZapiszTabliceDoPliku(tablica, "tablice/tablica1.txt");
 
     tablica.sortujBabelkowo();
 
+    // Odczytuje tablice z pliku aby kolejne sortowanie dzialalo na oryginalnej tablicy
     OdczytajDaneZPlikuDoTablicy(tablica, "tablice/tablica1.txt");
 
     tablica.sortujPrzezZliczanie();
 
+    // Zapisanie posortowanej tablicy do pliku
     ZapiszTabliceDoPliku(tablica, "tablice/wynik1.txt");
 
     system("pause");
